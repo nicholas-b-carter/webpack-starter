8d5ac4b55c0d17420a99f6dd5d0f88cb43479988
Phil Lgr <pleger2@gmail.com>
2017-04-06 22:36:51 -0400

    Output fonts, images etc. to assets folder

--- webpack-config/loaders/url.loader.js
+++ webpack-config/loaders/url.loader.js
@@ -1,4 +1,10 @@
 module.exports = {
     test: /\.((svg)(\?v=[0-9]\.[0-9]\.[0-9]))|(ico)$/,
-    use: 'url-loader?limit=100000',
+    use: {
+        loader: 'url-loader',
+        options: {
+            limit: 100000,
+            name: 'assets/[name].[hash].[ext]'
+        }
+    }
 };
